7d814adbefdc0f7cf47a9c4cd4b09d839f16e8a2
sheezzmee <106715792+sheezzmee@users.noreply.github.com>
2023-12-13 15:33:07 +0300

    Update cheatBase.user.js

--- cheatBase.user.js
+++ cheatBase.user.js
@@ -1,1 +1,20 @@
+// ==UserScript==
+// @name             Cheat Base
+// @author           sheezzmee, sabaka-babaka
+// @version          1.0.0
 
+// @author-discord   sheezzmee, sabaka_babaka, https://discord.gg/gMAJX2AdBS
+
+// @match            https://*.test-eu.tankionline.com/browser-public/index.html?*
+// @match            https://tankionline.com/play*
+
+// @icon             https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
+// @grant            unsafeWindow
+// @grant            GM_xmlhttpRequest
+// @connect          shizoval-site.vercel.app
+// @run-at           document-start
+// ==/UserScript==
+
+// очень важно!! не убирать!!
+window.xmlhttpRequest = GM_xmlhttpRequest;
+// скоро
